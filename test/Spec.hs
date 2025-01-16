652cd8bfa114f687b6e521b983ed773a68d16868
Matthew Herzl <matthew@herzl.email>
2025-01-16 13:36:32 -0600

    clean up warnings in test/Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,14 +6,17 @@ import Util (showText)
 import Data.Text
 import Test.HUnit
 
+main :: IO Counts
 main = runTestTT tests
 
+tests :: Test
 tests = TestList
   [ TestLabel "test1" test1
   , TestLabel "test2" test2
   , TestLabel "test3" test3
   ]
 
+test3 :: Test
 test3 = TestCase $ do
   let t = "aababcabcdabcdeabcdefabcdefgabcdefgh"
   let expected = ("abcd", 5)
@@ -21,6 +24,7 @@ test3 = TestCase $ do
   let actual = longestSubstringWithRRepeats r t
   assertEqual (unpack $ "longestSubstringWithRRepeats r=" <> showText r) expected actual
 
+test2 :: Test
 test2 = TestCase $ do
   let t = "abbcccddddeeeeeffffffggggggghhhhhhhh"
   let expected = ("hhhhh", 4)
@@ -28,6 +32,7 @@ test2 = TestCase $ do
   let actual = longestSubstringWithRRepeats r t
   assertEqual (unpack $ "longestSubstringWithRRepeats r=" <> showText r) expected actual
 
+test1 :: Test
 test1 = TestCase $ do
   let t = "abbcccddddeeeeeffffffggggggghhhhhhhh"
   let expected = ("h", 8)
